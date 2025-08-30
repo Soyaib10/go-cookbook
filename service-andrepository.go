e1e8918452b7822664de6a851659b3637dae4937
Soyaib10 <soyaibzihad10@gmail.com>
2025-08-31 00:22:26 +0600

    add server-repository classic example

---
+++ service-andrepository.go
@@ -0,0 +1,241 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/Soyaib10/popcorndb-api/internal/validator"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+type MovieModel struct {
+	DB *pgxpool.Pool
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	/*
+		- check for param err
+		- create a context
+		- write query
+		- get data from db
+		- bind data with destinaton struct
+		- handle possible errors
+	*/
+
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+	SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies
+	WHERE id = $1;
+	`
+	row := m.DB.QueryRow(ctx, query, id)
+
+	var movie Movie
+	err := row.Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		&movie.Genres,
+		&movie.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, pgx.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
+}
+
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
+	/*
+		- create a context
+		- write query
+		- get data from db and close rows
+		- bind data with destinaton struct
+		- handle possible errors
+	*/
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		ORDER BY id
+	`
+	rows, err := m.DB.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	movies := []*Movie{}
+	for rows.Next() {
+		var movie Movie
+		err := rows.Scan(
+			&movie.ID,
+			&movie.CreatedAt,
+			&movie.Title,
+			&movie.Year,
+			&movie.Runtime,
+			&movie.Genres,
+			&movie.Version,
+		)
+		if err != nil {
+			return nil, err
+		}
+		movies = append(movies, &movie)
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return movies, nil
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	/*
+		- create a context
+		- prepare arguments from struct
+		- execute query and return inserted row
+		- bind returned values into destination struct
+		- handle possible errors
+	*/
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version
+		`
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, movie.Genres}
+	row := m.DB.QueryRow(ctx, query, args...)
+
+	err := row.Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Version,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	/*
+		- create a context
+		- write query
+		- prepare arguments from struct
+		- execute the query (with RETURN maybe)
+		- bind returned values into destination struct
+		- handle possible errors
+	*/
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5 AND version = $6
+		RETURNING version
+	`
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		movie.Genres,
+		movie.ID,
+		movie.Version,
+	}
+	row := m.DB.QueryRow(ctx, query, args...)
+
+	err := row.Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	/*
+		- check param
+		- create a context 
+		- write query 
+		- execute the query using Exec()
+		- check how many rows were affected
+		- if no rows affected, return ErrRecordNotFound
+		- otherwise, return nil
+	*/
+
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		DELETE FROM movies
+		WHERE id = $1
+		`
+	result, err := m.DB.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected := result.RowsAffected()
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+}
