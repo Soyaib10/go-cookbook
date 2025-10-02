d82c5bbe6e69b93e01b16ce2e135381992d264e1
Soyaib10 <soyaibzihad10@gmail.com>
2025-10-02 22:11:00 +0600

    defer basic

---
+++ basics/defer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = f.WriteString("File content")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("File written successfully")
+}
