5f908be69ff9fc6bd64c5614b9895399bd91e07d
Soyaib10 <soyaibzihad10@gmail.com>
2025-10-15 23:54:00 +0600

    select statement

--- basics/channels/channels.go
+++ basics/channels/channels.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	basic()
 	buffer()
 	closingChannel()
+	selectStatement()
 }
 
 func basic() {
@@ -31,7 +33,6 @@ func buffer() {
 	fmt.Println(<-ch)
 }
 
-
 func closingChannel() {
 	ch := make(chan int)
 
@@ -44,5 +45,27 @@ func closingChannel() {
 
 	for v := range ch {
 		fmt.Println(v)
+	}
+}
+
+func selectStatement() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "Data from channel 1"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "Data from channel 2"
+	}()
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Println(msg1)
+	case msg2 := <-ch2:
+		fmt.Println(msg2)
 	}
 }
