8b0bffbb653c5ec4ced86e200a388ad8798f520c
Soyaib10 <soyaibzihad10@gmail.com>
2025-10-20 23:56:00 +0600

    race condition solving

--- basics/race-condition/race-condition.go
+++ basics/race-condition/race-condition.go
@@ -8,12 +8,15 @@ import (
 func main() {
 	var value int
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			mu.Lock()
 			value++
+			mu.Unlock()
 		}()
 	}
 
