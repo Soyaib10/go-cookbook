3e5d3e606727994a02632328107f801ac21de0c7
Soyaib10 <soyaibzihad10@gmail.com>
2025-10-21 00:21:00 +0600

    race contiion solve with chanell

--- basics/race-condition/race-condition.go
+++ basics/race-condition/race-condition.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	mutex()
+	channel()
+}
+
+func mutex() {
 	var value int
 	var wg sync.WaitGroup
 	var mu sync.Mutex
@@ -22,4 +27,30 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Value:", value)
+}
+
+func channel() {
+	const numGoroutines = 10
+
+	dataCh := make(chan int, numGoroutines)
+	var wg sync.WaitGroup
+
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			dataCh <- 1
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(dataCh)
+	}()
+
+	total := 0
+	for v := range dataCh {
+		total += v
+	}
+	fmt.Println("Data value:", total)
 }
